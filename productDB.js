1d8f3a05f8fd6bd15a301e5fb67c86288f20527f
Sp712409 <souvikpalme@gmail.com>
2023-02-12 22:46:33 +0530

    first commit

---
+++ productDB.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const connectDB= require("./db/connect");
+const product = require("./models/products");
+
+const productJson = require("./products.json");
+
+const start = async() => {
+    try{
+        await connectDB(process.env.MONGODB_URL);
+        await product.deleteMany();
+        await product.create(productJson);
+        console.log("success");
+
+    } catch(error){
+        console.log(error);
+
+    }
+};
+
+start();
